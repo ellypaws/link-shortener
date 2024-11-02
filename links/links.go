340fb551657480eb3583b4ad9e0c76d68d6f78bc
Elly <145195760+ellypaws@users.noreply.github.com>
2024-11-02 09:31:00 +0800

    feat: initial commit backend

---
+++ links/links.go
@@ -0,0 +1,172 @@
+package links
+
+import (
+	"encoding/json"
+	"math/rand"
+	"os"
+	"time"
+)
+
+type Linker struct {
+	Users map[string]*User
+	Links map[string]*ShortLink
+}
+
+func NewLinker() *Linker {
+	l := new(Linker)
+	l.ReloadLinks()
+	return l
+}
+
+type ShortLink struct {
+	Short       string     `json:"short,omitempty"`
+	Original    string     `json:"original,omitempty"`
+	Owner       string     `json:"owner,omitempty"`
+	DateCreated time.Time  `json:"date_created"`
+	DateExpired *time.Time `json:"date_expired,omitempty"`
+}
+
+type Role = int
+
+const (
+	RoleAdmin Role = iota
+	RoleUser
+)
+
+type User struct {
+	Username string
+	Email    string
+	Password string
+	Role     Role
+}
+
+func (l *Linker) GetLink(short string) *ShortLink {
+	if l.Links == nil {
+		l.ReloadLinks()
+	}
+	return l.Links[short]
+}
+
+func (l *Linker) NewLink(original string, opts ...func(*ShortLink)) ShortLink {
+	link := ShortLink{
+		Original:    original,
+		DateCreated: time.Now(),
+	}
+
+	for _, f := range opts {
+		f(&link)
+	}
+
+	return link
+}
+
+func WithShort(short string) func(*ShortLink) {
+	if short == "" {
+		short = generateShortLink()
+	}
+	return func(link *ShortLink) {
+		link.Short = short
+	}
+}
+
+func WithRandomShort() func(*ShortLink) {
+	return func(link *ShortLink) {
+		link.Short = generateShortLink()
+	}
+}
+
+func WithOwner(owner User) func(*ShortLink) {
+	return func(link *ShortLink) {
+		link.Owner = owner.Username
+	}
+}
+
+func (l *Linker) AddLink(link ShortLink) error {
+	if l.Links == nil {
+		l.ReloadLinks()
+	}
+	if link.Short == "" {
+		link.Short = generateShortLink()
+	}
+
+	l.Links[link.Short] = &link
+
+	return l.SaveLinks()
+}
+
+func generateShortLink() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func (l *Linker) SaveLinks() error {
+	f, err := os.Create("links.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	shorts := make([]*ShortLink, 0, len(l.Links))
+	for _, link := range l.Links {
+		shorts = append(shorts, link)
+	}
+
+	return json.NewEncoder(f).Encode(shorts)
+}
+
+func (l *Linker) ReloadLinks() {
+	var err error
+	l.Links, err = loadLinks()
+	if err != nil {
+		l.Links = make(map[string]*ShortLink)
+	}
+
+	l.Users = map[string]*User{
+		"admin": {
+			Username: "admin",
+			Email:    "admin@mail.com",
+			Password: "root",
+			Role:     RoleAdmin,
+		},
+	}
+}
+
+func loadLinks() (map[string]*ShortLink, error) {
+	_, err := os.Stat("links.json")
+	if err != nil {
+		return nil, createLinksFile()
+	}
+
+	f, err := os.Open("links.json")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var shorts []*ShortLink
+	err = json.NewDecoder(f).Decode(&shorts)
+	if err != nil {
+		return nil, err
+	}
+
+	linksMap := make(map[string]*ShortLink)
+	for _, link := range shorts {
+		linksMap[link.Short] = link
+	}
+
+	return linksMap, nil
+}
+
+func createLinksFile() error {
+	f, err := os.Create("links.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return json.NewEncoder(f).Encode([]ShortLink{})
+}
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"link-shortener/links"
+	"net/http"
+)
+
+type App struct {
+	Links *links.Linker
+	echo  *echo.Echo
+}
+
+func NewApp() *App {
+	return &App{
+		Links: links.NewLinker(),
+		echo:  echo.New(),
+	}
+}
+
+func main() {
+	app := NewApp()
+
+	app.echo.Use(middleware.Logger())
+
+	app.echo.Use(app.storeLinker)
+	app.echo.GET("/:short", retrieveLink)
+	app.echo.POST("/shorten", shortenLink)
+
+	app.echo.Logger.Fatal(app.echo.Start(":1323"))
+}
+
+func (app App) storeLinker(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Set("linker", app.Links)
+		return next(c)
+	}
+}
+
+func retrieveLink(c echo.Context) error {
+	short := c.Param("short")
+	linker, ok := c.Get("linker").(*links.Linker)
+	if !ok {
+		return c.String(500, "linker not found")
+	}
+	link := linker.GetLink(short)
+	if link == nil {
+		return c.String(404, "Link not found")
+	}
+
+	return c.Redirect(http.StatusTemporaryRedirect, link.Original)
+}
+
+func shortenLink(c echo.Context) error {
+	link := links.ShortLink{
+		Original: c.FormValue("original"),
+		Short:    c.FormValue("short"),
+	}
+
+	linker, ok := c.Get("linker").(*links.Linker)
+	if !ok {
+		return c.String(500, "linker not found")
+	}
+
+	short := linker.NewLink(link.Original, links.WithShort(link.Short))
+	err := linker.AddLink(short)
+	if err != nil {
+		return c.String(500, "Failed to add link")
+	}
+
+	return c.JSON(200, link)
+}
